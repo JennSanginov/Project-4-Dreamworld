86dd9474c77183ebbbedd27d359eed812320b09d
JennSanginov <jsanginov@gmail.com>
2022-11-18 12:50:34 -0700

    Second File

---
+++ src/com/neotech/project1/StudyGroupATeam.java
@@ -0,0 +1,20 @@
+package com.neotech.project1;
+
+public class StudyGroupATeam {
+
+	//// Hello Study GROUP aka A Team :-)) //// . //Interview Question.
+	public static void main(String[] args) {
+
+		int num1 = 20;
+
+		if (num1 % 2 == 0) {
+			System.out.println("Given number " + num1 + " is an even number.");
+
+		} else {
+
+			System.out.println(" Given number" + num1 + " is an odd number.");
+		}
+
+	}
+
+}
